ce240734f67245b6de5d598b08194e6c3efe18e6
root <root@Khumraj11>
2023-12-29 20:52:38 +0545

    my first commit

---
+++ src/instructions.rs
@@ -0,0 +1,31 @@
+use borsh::BorshDeserialize;
+use borsh_derive::{BorshDeserialize, BorshSerialize};
+use solana_program::program_error::ProgramError;
+
+#[derive(Debug, BorshDeserialize, BorshSerialize)]
+pub struct CountArgs {
+    pub value: u32,
+}
+
+pub enum CounterInstructions {
+    Increment(CountArgs),
+    Decrement(CountArgs),
+    Update(CountArgs),
+    Reset,
+}
+
+impl CounterInstructions {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (&variant, rest) = input
+            .split_first()
+            .ok_or(ProgramError::InvalidInstructionData)?;
+
+        Ok(match variant {
+            0 => Self::Increment(CountArgs::try_from_slice(rest).unwrap()),
+            1 => Self::Decrement(CountArgs::try_from_slice(rest).unwrap()),
+            2 => Self::Update(CountArgs::try_from_slice(rest).unwrap()),
+            3 => Self::Reset,
+            _ => return Err(ProgramError::InvalidInstructionData),
+        })
+    }
+}
